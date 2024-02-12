26a2774aee8ccfe9f13d6f3897bd4694ba29c726
Emil Rundgren <144038584+Emil-Rundgren@users.noreply.github.com>
2024-02-12 23:38:59 +0100

    Jag la till ett färdigt formulär i LogInView från ett annat projekt samt Började jag addera inehåll på hemsidan. Med axios hämtade jag Information från API NewsAPI och la in informatio dynamiskt in i ett v-card. Jag implementerade: {{och}} v-bind samt axios

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,35 @@
 <script>
 import axios from 'axios'
-// Want to use async/await? Add the `async` keyword to your outer function/method.
+
 export default {
+  data() {
+    return {
+      newsItems: []
+    }
+  },
+  async created() {
+    await this.getNews()
+  },
   methods: {
-    async getUser() {
+    // Get request
+    async getNews() {
       try {
         const response = await axios.get(
-          'https://newsapi.org/v2/everything?q=tesla&apiKey=65198de0fc7e418eb3db417f3e104479'
+          'https://newsapi.org/v2/top-headlines?country=se&apiKey=65198de0fc7e418eb3db417f3e104479'
         )
-        console.log(response)
+        this.news = response.data.articles
+        // Add data to newsItems after news data is fetched
+        if (this.news && this.news.length > 0) {
+          // article represents an individual element (object)
+          // In each article there are three key newsImg...
+          // If fetched value is falsy(undifined) add default value instead
+          this.newsItems = this.news.map((article) => ({
+            newsImg: article.urlToImage || './public/neom-WLeWJW_WneE-unsplash.jpeg',
+            newsTitle: article.title || 'No title available',
+            newsDescription: article.description || 'No description available'
+          }))
+        }
+        console.log(this.newsItems)
       } catch (error) {
         console.error(error)
       }
@@ -17,12 +38,28 @@ export default {
 }
 </script>
 
+<!-- v-if="newsItems.length > 0" is needed otherwise the template will try to access the fetced data before it has been fetched -->
 <template>
   <v-container fluid>
     <v-row justify="center">
       <v-col cols="8" class="d-flex justify-space-between align-center">
-        <h1>This is the homepage</h1>
-        <v-btn variant="flat" color="black" @click="getUser">Klicka</v-btn>
+        <h1>World News</h1>
+        <v-card>
+          <v-img
+            height="200px"
+            cover
+            v-if="newsItems.length > 0"
+            :src="newsItems[0].newsImg"
+          ></v-img>
+
+          <v-card-title v-if="newsItems.length > 0">
+            {{ newsItems[0].newsTitle }}
+          </v-card-title>
+
+          <v-card-subtitle v-if="newsItems.length > 0">
+            {{ newsItems[0].newsDescription }}</v-card-subtitle
+          >
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
--- src/views/LogInView.vue
+++ src/views/LogInView.vue
@@ -1,8 +1,87 @@
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: '',
+        password: '',
+        remember: false
+      },
+      isLoading: false,
+      rules: {
+        required: (value) => !!value || 'Required',
+        counter: (value) => value.length <= 20 || 'Max 20 characters',
+        email: (value) => {
+          const pattern =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          return pattern.test(value) || 'Invalid e-mail.'
+        }
+      }
+    }
+  },
+
+  methods: {
+    submit() {
+      // Cancels the function if the user hasn't entered anything
+      if (this.form.email === '') {
+        return
+      }
+      // If the user has entered
+      this.isLoading = true
+      setTimeout(() => {
+        this.isLoading = false
+        alert(JSON.stringify(this.form))
+      }, 3000)
+    }
+  }
+}
+</script>
+
 <template>
   <v-container fluid>
+    <v-overlay :model-value="isLoading">
+      <v-progress-circular v-if="isLoading" indeterminate color="white"></v-progress-circular>
+    </v-overlay>
     <v-row justify="center">
-      <v-col cols="8" class="d-flex justify-space-between align-center">
-        <h1>This is a Log In page</h1>
+      <v-col cols="4">
+        <v-card class="pa-4">
+          <v-card-title class="text-center text-h5 font-weight-bold"
+            >Log in or create an account</v-card-title
+          >
+          <v-card-item>
+            <v-form @submit.prevent="submit">
+              <v-text-field
+                v-model="form.email"
+                :rules="[rules.required, rules.email]"
+                label="Email"
+                prepend-inner-icon="mdi-email"
+              ></v-text-field>
+              <v-text-field
+                type="password"
+                :rules="[rules.required]"
+                v-model="form.password"
+                label="Password"
+                prepend-inner-icon="mdi-key"
+              ></v-text-field>
+
+              <v-checkbox
+                v-model="form.remember"
+                label="Remeber Me"
+                color="black"
+                hide-details
+              ></v-checkbox>
+
+              <v-btn color="black" type="submit" block class="mt-2" variant="elevated">
+                Submit
+              </v-btn>
+            </v-form>
+          </v-card-item>
+          <v-card-action>
+            <div class="mx-4">
+              <v-btn block flat variant="outlined" to="/register">Register</v-btn>
+            </div>
+          </v-card-action>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
